9acdc9295442727070a6d12e8a74c69367cebb14
GravityKat <mekail578@gmail.com>
2024-07-24 21:49:03 -0600

    done!

--- index.js
+++ index.js
@@ -21,3 +21,22 @@ formattedCitations.forEach(citation => {
   citationElement.textContent = citation;
   citationsList.appendChild(citationElement);
 });
+
+// Sustainability Dropdown
+
+document.addEventListener('DOMContentLoaded', function () {
+  const dropdownButtons = document.querySelectorAll('.dropdown-btn');
+
+  dropdownButtons.forEach(button => {
+    button.addEventListener('click', function () {
+      this.classList.toggle('active');
+      const dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+      }
+    });
+  });
+});
+
